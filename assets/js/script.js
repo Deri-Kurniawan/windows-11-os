0a689ebcf2dd6ac4236c3f7ccdbc46e7a563efc3
Deri Kurniawan <deri.netuchi@gmail.com>
2021-07-12 19:28:47 +0700

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,6 @@
     // set var
-
     var body = document.body;
-
     var navbar = document.getElementById('navbar');
-
     var changeMode = document.getElementById('change-mode');
 
     // Check Last Mode
@@ -11,79 +8,50 @@
     // night to day 
     if (localStorage['nightMode'] === 'off') {
         changeMode.setAttribute('title', 'Set To Night Mode');
-        
         changeMode.classList.replace('fa-sun', 'fa-moon');
-        
         navbar.classList.replace('night-mode', 'bg-light');
-        
         navbar.classList.replace('navbar-dark', 'navbar-light');
-        
         body.classList.remove('night-mode');
-        
         console.log('Recent Night Mode Off');
-
         // day to night
     } else if (localStorage['nightMode'] === 'on') {
         changeMode.setAttribute('title', 'Set To Day Mode');
-
         changeMode.classList.replace('fa-moon', 'fa-sun');
-
         navbar.classList.replace('bg-light', 'night-mode');
-
         navbar.classList.replace('navbar-light', 'navbar-dark');
-
         body.classList.add('night-mode');
-
         console.log('Recent Night Mode On');
-
     }
 
     // add event
-
     changeMode.addEventListener('click', function () {
-        
         // check last class name
         
         // activation night mode
         if (navbar.classList[1] == 'navbar-light') {
-
             navbar.classList.replace('bg-light', 'night-mode');
-
             navbar.classList.replace('navbar-light', 'navbar-dark');
-
             changeMode.classList.replace('fa-moon', 'fa-sun');
-
             changeMode.setAttribute('title', 'Set To Day Mode');
-
             navbar.classList.replace('navbar-light', 'navbar-dark');
-
             body.classList.add('night-mode');
 
             // set night mode to on
-
             localStorage['nightMode'] = 'on';
-
             console.log('Night Mode On');
 
             // in-activated night mode
         } else if (navbar.classList[1] == 'navbar-dark') {
 
             navbar.classList.replace('night-mode', 'bg-light');
-
             navbar.classList.replace('navbar-dark', 'navbar-light');
-
             changeMode.setAttribute('title', 'Set To Night Mode');
-
             changeMode.classList.replace('fa-sun', 'fa-moon');
-
             body.classList.remove('night-mode');
 
             // set night mode to off
-
             localStorage['nightMode'] = 'off';
-
             console.log('Night Mode Off');
-
         }
 
         // console.log(localStorage['nightMode']);
@@ -93,6 +61,7 @@
 
     // navigation bar
     window.addEventListener('scroll', function(){ 
+
         if(window.pageYOffset >= 650){
             navbar.classList.add('nav-scroll');
         } else if(window.pageYOffset <= 0){
@@ -119,19 +88,11 @@ $("#typewriter").typer({
     "Saya Deri Kurniawan",
     "Saya Seorang Developer!"
   ],
-
   typeSpeed: 60,
-
   backspaceSpeed: 20,
-
   backspaceDelay: 800,
-
   repeatDelay: 500,
-
   repeat:true,
-
   autoStart:true,
-
   startDelay: 100,
-
 });
