48c247cd0c1d43b1a187321d9c6f036e308f5605
deri-kurniawan <deri.netuchi@gmail.com>
2021-09-21 14:21:45 +0700

    clear code and adding redirect feature

---
+++ assets/js/redirect.js
@@ -0,0 +1,8 @@
+const urlString = window.location.href;
+const urlTarget = new URL(urlString).searchParams.get("to");
+
+console.log(urlTarget)
+
+if (urlTarget !== null && urlTarget !== '') {
+    location.href = `https://www.${urlTarget}`;
+}
