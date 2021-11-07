da7772edd2de8e2800d868d7d9a08083cbb7f139
Deri Kurniawan <deri.netuchi@gmail.com>
2021-11-07 11:42:28 +0700

    Update redirect.js

--- v1/assets/js/redirect.js
+++ v1/assets/js/redirect.js
@@ -1,8 +1,6 @@
 const urlString = window.location.href;
 const urlTarget = new URL(urlString).searchParams.get("to");
 
-console.log(urlTarget)
-
 if (urlTarget !== null && urlTarget !== '') {
     location.href = `https://${urlTarget}`;
 }
