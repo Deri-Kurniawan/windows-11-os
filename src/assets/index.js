5191337d430306d714f80cd3c5c9e7536f4ec228
Deri Kurniawan <deri.netuchi@gmail.com>
2022-08-13 03:13:47 +0700

    Feat: Lock Screen
    Added:
    - Packages: moment js
    - Tailwindcss config screens size
    - Images: Wallpaper, Profile
    - Reducer props
    
    Implement: Lock screen Component for strict access to Desktop Screen

---
+++ src/assets/index.js
@@ -0,0 +1,20 @@
+const ASSETS = {
+  images: {
+    profile: {
+      require: require("./images/profile.jpg"),
+    },
+    wallpapers: [
+      {
+        require: require("./images/wallpapers/1.png"),
+      },
+      {
+        require: require("./images/wallpapers/2.png"),
+      },
+      {
+        require: require("./images/wallpapers/3.png"),
+      },
+    ],
+  },
+};
+
+export default ASSETS;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import desktopReducer from "./feat/desktopSlice";
+import lockScreenReducer from "./feat/lockScreenSlice";
 
 const store = configureStore({
   reducer: {
     desktop: desktopReducer,
+    lockScreen: lockScreenReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
