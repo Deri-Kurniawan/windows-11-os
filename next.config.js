dc104bdff7341bd5ac33a19d3950f3bce170b852
Deri Kurniawan <deri.netuchi@gmail.com>
2022-12-01 01:30:29 +0700

    Feat(Configs): Ignore TS Error on Build, Enable Next Experimental appDir

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    appDir: true,
+  },
+};
